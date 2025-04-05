1eff7550351bc85f09f9a6226c9c70943e0fc1df
Krish Kumar <indokrisgraphics@gmail.com>
2025-04-05 21:34:10 +0530

    Add files via upload

---
+++ cursor.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+while True:
+    a = pyautogui.position()
+    print(a)
+
+
+    # 1125,989 location of wp
+    # top - 450 60 
+    # bottom - 1406,926
+    # 560,957 #keyboard
+    # 510,958 - keyboard
+    # 1000,200 - click to remove copied selection
---
+++ geminiAPI.py
@@ -0,0 +1,20 @@
+
+# THIS IS A WORKING API , But showing error in the end
+
+import google.generativeai as genai
+import absl.logging
+absl.logging.set_verbosity(absl.logging.ERROR)
+
+
+
+genai.configure(api_key="AIzaSyAxKfQWv_sXXFWsb2EVuLHxbXilI4QpgFg")  # Replace with your actual API key
+
+# Load the correct model
+model = genai.GenerativeModel("models/gemini-1.5-pro-002")  # Ensure this matches an available model
+
+# Now, generate content
+response = model.generate_content("Hello,my assistant !")
+
+# Print the response
+print(response.text)
+
